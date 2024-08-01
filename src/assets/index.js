21a6209ee91772b9a720fbd15fdcb60a3f6fa672
James <jamesarcdev@gmail.com>
2024-08-01 05:18:27 -0700

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,136 @@
+import bwmap from './backgrounds/bg-me.jpg';
+import nairobi from './backgrounds/nairobi.png';
+import whiteabstract from './backgrounds/white-abstract.png';
+import worldmap from './backgrounds/world-map.png';
+
+import logotext from './logo/logo-no-background.png';
+
+import backend from './icons/backend.png';
+import ux from './icons/ux.png';
+import frontend from './icons/frontend.png';
+import prototyping from './icons/prototyping.png';
+import github from './icons/github.png';
+import close from './icons/close.png';
+import download from './icons/download.png';
+import downloadHover from './icons/downloadHover.png';
+import pineapple from './icons/pineapple.png';
+import pineappleHover from './icons/pineappleHover.png';
+import menu from './icons/menu.png';
+import resume from './icons/resume.png';
+import send from './icons/send.png';
+import sendHover from './icons/sendHover.png';
+
+import css from './tech/css.png';
+import docker from './tech/docker.png';
+import figma from './tech/figma.png';
+import git from './tech/git.png';
+import html from './tech/html.png';
+import javascript from './tech/javascript.png';
+import nodejs from './tech/nodejs.png';
+import reactjs from './tech/reactjs.png';
+import redux from './tech/redux.png';
+import tailwind from './tech/tailwind.png';
+import typescript from './tech/typescript.png';
+import graphql from './tech/graphql.png';
+import postgresql from './tech/postgresql.png';
+import csharp from './tech/csharp.png';
+import angular from './tech/angular.png';
+import java from './tech/java.png';
+import aws from './tech/aws.png';
+import laravel from './tech/laravel.png';
+import vue from './tech/vue.png';
+import spring_java from './tech/spring.png';
+import mongo from './tech/mongo.png';
+
+import coverhunt from './company/coverhunt.png';
+import dcc from './company/dcc.png';
+import kelhel from './company/kelhel.png';
+import microverse from './company/microverse.png';
+
+import komikult from './projects/komikult.png';
+import leaderboard from './projects/leaderboard.png';
+import math from './projects/math-magicians.png';
+import movie from './projects/movie-metro.png';
+import nyeusi from './projects/nyeusi.png';
+import space from './projects/space-hub.png';
+import pokt from './projects/pokt.png';
+import classdojo from './projects/classdojo.png';
+import chewy from './projects/chewy.png';
+import chegg from './projects/chegg.png';
+import poshmark from './projects/poshmark.png';
+
+import poktpool_com from './company/poktpool.jpg';
+import classdojo_com from './company/classdojo-removebg-preview.png';
+import coupa_com from './company/coupa.png';
+import infurm_com from './company/infurm.jpg';
+import intellisoft_com from './company/intellisoft.jpg';
+import shoclef_com from './company/shoclef.jpg';
+import reno_com from './company/Reno.png';
+
+import resume_pdf from './resume/JamesYoungResume.pdf';
+
+export {
+  bwmap,
+  nairobi,
+  whiteabstract,
+  worldmap,
+  logotext,
+  backend,
+  ux,
+  frontend,
+  prototyping,
+  github,
+  close,
+  download,
+  downloadHover,
+  pineapple,
+  pineappleHover,
+  menu,
+  resume,
+  send,
+  sendHover,
+  css,
+  docker,
+  figma,
+  git,
+  graphql,
+  html,
+  javascript,
+  nodejs,
+  postgresql,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  komikult,
+  leaderboard,
+  math,
+  movie,
+  nyeusi,
+  space,
+  coverhunt,
+  dcc,
+  kelhel,
+  microverse,
+  csharp,
+  mongo,
+  angular,
+  vue,
+  java,
+  spring_java,
+  aws,
+  laravel,
+  pokt,
+  chegg,
+  chewy,
+  classdojo,
+  poshmark,
+  poktpool_com,
+  classdojo_com,
+  shoclef_com,
+  infurm_com,
+  intellisoft_com,
+  coupa_com,
+  reno_com,
+  resume_pdf
+};
---
+++ src/constants/index.js
@@ -0,0 +1,301 @@
+import { spring } from 'framer-motion';
+import {
+  frontend,
+  backend,
+  ux,
+  prototyping,
+  javascript,
+  typescript,
+  reactjs,
+  tailwind,
+  nodejs,
+  git,
+  docker,
+  postgresql,
+  graphql,
+  csharp,
+  aws,
+  mongo,
+  java,
+  laravel,
+  angular,
+  vue,
+  pokt,
+  classdojo,
+  chegg,
+  classdojo_com,
+  intellisoft_com,
+  shoclef_com,
+  infurm_com,
+  coupa_com,
+  poktpool_com,
+  reno_com,
+  poshmark,
+  chewy,
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'Frontend Developer',
+    icon: frontend,
+  },
+  {
+    title: 'Backend Developer',
+    icon: backend,
+  },
+  {
+    title: 'UI/UX Design',
+    icon: ux,
+  },
+  {
+    title: 'Software Prototyping',
+    icon: prototyping,
+  },
+];
+
+const technologies = [
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'TypeScript',
+    icon: typescript,
+  },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Angular',
+    icon: angular
+  },
+  {
+    name: 'Vue',
+    icon: vue
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: tailwind,
+  },
+  {
+    name: 'Node JS',
+    icon: nodejs,
+  },
+  {
+    name: 'C#',
+    icon: csharp,
+  },
+  {
+    name: 'Java',
+    icon: java
+  },
+  {
+    name: 'Laravel',
+    icon: laravel
+  },
+  {
+    name: 'Graphql',
+    icon: graphql,
+  },
+  {
+    name: 'Postgresql',
+    icon: postgresql,
+  },
+  {
+    name: 'Mongodb',
+    icon: mongo
+  },
+  {
+    name: 'Git',
+    icon: git,
+  },
+  {
+    name: 'Docker',
+    icon: docker,
+  },
+  {
+    name: 'AWS',
+    icon: aws
+  }
+];
+
+const experiences = [
+  {
+    title: 'B.A. in Computer Science',
+    company_name: 'University of Nevada - Reno',
+    icon: reno_com,
+    iconBg: '#333333',
+    date: 'Aug 2009 - July 2012',
+  },
+  {
+    title: 'FullStack Developer',
+    company_name: 'Coupa Software',
+    icon: coupa_com,
+    iconBg: '#333333',
+    date: 'Sept 2012 - Nov 2014',
+  },
+  {
+    title: 'Senior FullStack Developer',
+    company_name: 'Infurm Technologies',
+    icon: infurm_com,
+    iconBg: '#333333',
+    date: ' Nov 2014 - April 2017',
+  },
+  {
+    title: 'Senior Frontend Developer',
+    company_name: 'Shoclef',
+    icon: shoclef_com,
+    iconBg: '#333333',
+    date: 'May 2017 - Feb 2019',
+  },
+  {
+    title: 'Web3 FullStack Developer',
+    company_name: 'IntelliSoft',
+    icon: intellisoft_com,
+    iconBg: '#333333',
+    date: 'Mar 2019 - May 2020',
+  },
+  {
+    title: 'Senior Frontend Developer',
+    company_name: 'ClassDojo',
+    icon: classdojo_com,
+    iconBg: '#333333',
+    date: 'June 2020 - July 2022',
+  },
+  {
+    title: 'Lead Front-End Developer',
+    company_name: 'poktpool',
+    icon: poktpool_com,
+    iconBg: '#333333',
+    date: 'June 2024 - July 2022',
+  },
+];
+
+const projects = [
+  {
+    id: 'project-1',
+    name: 'poktpool',
+    description: 'poktpool is a staking pool for POKT tokens, offering a platform for users to earn rewards.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'mongodb',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'tailwind',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: pokt,
+    demo: 'https://poktpool.com',
+  },
+  {
+    id: 'project-2',
+    name: 'ClassDojo',
+    description:
+      'ClassDojo connects primary school teachers, students and \
+      families through communication features, such as a feed for photos and videos from the school day.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'restapi',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'scss',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: classdojo,
+    demo: 'https://classdojo.com/',
+  },
+  {
+    id: 'project-3',
+    name: 'Chegg',
+    description: 'If we’re successful, we can change the outcomes of millions of people.',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: chegg,
+    demo: 'https://www.chegg.com/',
+  },
+  {
+    id: 'project-4',
+    name: 'chewy',
+    description: `We’re here to stick by your side through the highs, lows and in-betweens of pet parenthood, with everything you and your pet need for a happy and healthy life. At Chewy, our mission is to be the most trusted and convenient destination for pet parents and partners, everywhere.`,
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: chewy,
+    demo: 'https://chewy.com/',
+  },
+  {
+    id: 'project-5',
+    name: 'poshmark',
+    description:
+      'Poshmark is a leading fashion resale marketplace powered by a vibrant, highly engaged community of buyers and sellers and real-time social experiences.',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: poshmark,
+    demo: 'https://poshmark.com',
+  },
+];
+
+export { services, technologies, experiences, projects };
